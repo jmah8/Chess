de3dfe1d9129230c03026efcfddbc98339c2f4b2
jonMa99 <jonathanmah99@gmail.com>
2020-05-24 16:27:19 -0700

    Made instruction pane not editable

--- src/main/ui/chessEditorGUI/InstructionPaneEditor.java
+++ src/main/ui/chessEditorGUI/InstructionPaneEditor.java
@@ -8,6 +8,7 @@ public class InstructionPaneEditor {
 
     public InstructionPaneEditor() {
         instructionPanel = new JTextPane();
+        instructionPanel.setEditable(false);
         instructionPanel.setSize(750, 100);
         instructionPanel.setFont(new java.awt.Font("Ubuntu", 1, 20));
         instructionPanel.setText("Clicking a piece on the right of the board allows you to press any" +
